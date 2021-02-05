8fb1728a431d5e81ea2046cb92732bb9297bbaf9
ZhuoyaoChen23 <zhuoyaoc@usc.edu>
2021-02-05 09:06:39 +0000

    2/4 midnight prototype

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -21,18 +21,33 @@ import time
 sys.path.append('../../Software/Python/')
 # This append is to support importing the LCD library.
 sys.path.append('../../Software/Python/grove_rgb_lcd')
-
+from grove_rgb_lcd import*
 import grovepi
-
+setRGB(0,255,0)
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
 if __name__ == '__main__':
-    PORT = 4    # D4
-
+    PORT = 4    # D4: port connects sensor
+    PORT_rot = 0; # A0: port connects encoder
+    grovepi.pinMode(PORT_rot,"INPUT") # new code
+    message = " " # string contains message to be displayed on LCD
+    flag_obj_pres = 0 # flag: when there's object within threshold, set to 1; otherwise set to 0
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
-
-        print(grovepi.ultrasonicRead(PORT))
+	#print(grovepi.analogRead(PORT_rot)) # test code: print encoder reading
+	print(grovepi.ultrasonicRead(PORT)) # print sensor reading on terminal
+	if grovepi.analogRead(PORT_rot) < grovepi.ultrasonicRead(PORT): # if no object within threshold
+		if flag_obj_pres == 1: # if there was object within threshold
+			setText(" ") # clear display
+		flag_obj_pres = 0
+		setText_norefresh("%3dcm\n%3dcm" % (grovepi.ultrasonicRead(PORT),grovepi.analogRead(PORT_rot)))
+		setRGB(0,255,0)
+	else:
+		if flag_obj_pres == 0: # if there was object within threshold
+			setText(" ") # clear display
+		flag_obj_pres = 1
+		setText_norefresh("%3dcm %s\n%3dcm" % (grovepi.ultrasonicRead(PORT),"OBJ PRES",grovepi.analogRead(PORT_rot)))
+		setRGB(255,0,0)
